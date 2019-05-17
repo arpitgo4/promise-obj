002402d2fa4f109462c78089039a7995df2f1381
Arpit Goyal <arpit.go4@gmail.com>
2019-05-17 12:11:04 +0530

    test cases added for Promise.Obj method

--- test/index.js
+++ test/index.js
@@ -1,8 +1,11 @@
 
 const promise_obj = require('../index');
 
-const { expect, } = require('chai');
+const chai = require('chai');
+const chaiAsPromised = require("chai-as-promised");
 
+const { expect } = chai;
+chai.use(chaiAsPromised);
 
 describe('Promise.obj()', () => {
 
@@ -57,4 +60,58 @@ describe('Promise.obj()', () => {
         expect(result_obj).to.not.equal(test_obj);
     });
 
+    it('should resolve all promises in given simple object', () => {
+        const test_obj = {
+            one: 1,
+            two: 2,
+            three: 3,
+        };
+
+        expect(promise_obj(test_obj)).to.eventually.equal(test_obj);
+    });
+
+    it('should resolve all promises in given complex objects ', () => {
+        const p3 = Promise.resolve(3);
+        const p5 = Promise.resolve(5);
+        const p7 = Promise.resolve(7);
+        const p9 = Promise.resolve(9);
+        const p10 = Promise.resolve(10);
+        const p12 = Promise.resolve(12);
+        const p13 = Promise.resolve(13);
+        const p15 = Promise.resolve(15);
+        const p16 = Promise.resolve(16);
+        
+
+        const test_obj = {
+            one: 1,
+            two: {
+                three: p3,
+                four: {
+                    five: p5,
+                    six: {
+                        seven: p7,
+                        eight: [ 8, p9, ],
+                    },
+                }
+            },
+            ten: [ p10, 11, p12, { thirteen: p13, fourteen: { fifteen: p15 } }, { sixteen: p16 } ],
+        };
+
+        const result_obj = {
+            one: 1,
+            two: {
+                three: 3,
+                four: {
+                    five: 5,
+                    six: {
+                        seven: 7,
+                        eight: [ 8, 9, ],
+                    },
+                }
+            },
+            ten: [ 10, 11, 12, { thirteen: 13, fourteen: { fifteen: 15 } }, { sixteen: 16 } ],
+        };
+
+        expect(promise_obj(test_obj)).to.eventually.deep.equals(result_obj);
+    });
 });
